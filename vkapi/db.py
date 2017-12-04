3eab477488a125c136cd7526ceccd94ead27fc3d
alega19 <alega19@mail.ru>
2017-12-04 18:23:21 +1000

    extract transaction_mode to decorator

--- vkapi/db.py
+++ vkapi/db.py
@@ -6,6 +6,29 @@ from vkapi.config import DATABASE
 from vkapi.config import DELAYS_BEFORE_RECONNECT_TO_DB
 
 
+def _use_transaction_mode(fn):
+    def wrapper(self, *args, **kwargs):
+        if self._transaction_mode:
+            return fn(self, *args, **kwargs)
+        else:
+            exc = None
+            for delay in DELAYS_BEFORE_RECONNECT_TO_DB:
+                try:
+                    if self._conn is None:
+                        self._connect()
+                    result = fn(self, *args, **kwargs)
+                    self._conn.commit()
+                    return result
+                except psycopg2.Error as err:
+                    logging.warning(repr(err))
+                    exc = err
+                    self._rollback()
+                    self.close()
+                    time.sleep(delay)
+            raise exc
+    return wrapper
+
+
 class Database:
 
     _conn = None
@@ -32,53 +55,15 @@ class Database:
                                       dbname=DATABASE['dbname'], user=DATABASE['user'],
                                       password=DATABASE['password'])
 
+    @_use_transaction_mode
     def execute(self, sql, params=None, handler=None):
-        if self._transaction_mode:
-            return self._execute(sql, params, handler)
-        else:
-            exc = None
-            for delay in DELAYS_BEFORE_RECONNECT_TO_DB:
-                try:
-                    if self._conn is None:
-                        self._connect()
-                    result = self._execute(sql, params, handler)
-                    self._conn.commit()
-                    return result
-                except psycopg2.Error as err:
-                    logging.warning(repr(err))
-                    exc = err
-                    self._rollback()
-                    self.close()
-                    time.sleep(delay)
-            raise exc
-
-    def _execute(self, sql, params=None, handler=None):
         with self._conn.cursor() as cursor:
             cursor.execute(sql, params)
             if handler is not None:
                 return [handler(row) for row in cursor]
 
+    @_use_transaction_mode
     def executemany(self, sql, params_list):
-        if self._transaction_mode:
-            self._executemany(sql, params_list)
-        else:
-            exc = None
-            for delay in DELAYS_BEFORE_RECONNECT_TO_DB:
-                try:
-                    if self._conn is None:
-                        self._connect()
-                    self._executemany(sql, params_list)
-                    self._conn.commit()
-                    return
-                except psycopg2.Error as err:
-                    logging.warning(repr(err))
-                    exc = err
-                    self._rollback()
-                    self.close()
-                    time.sleep(delay)
-            raise exc
-
-    def _executemany(self, sql, params_list):
         with self._conn.cursor() as cursor:
             cursor.executemany(sql, params_list)
 
